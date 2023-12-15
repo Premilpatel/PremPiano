0226b7a4db0412325de1ef3c04d8c2e117a29881
Premil Patel <127985686+Premilpatel@users.noreply.github.com>
2023-12-14 18:44:48 -0600

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Which raag: Sa Re Ma Pa, Ma Pa Dha Ni, Sa?",
+        answer: "B. Raag Yaman",
+        options: [
+            "A. Raag Bhimpalasi",
+            "B. Raag Yaman",
+            "C. Raag Bhairav",
+            "D. Raag Malkauns"
+        ]
+    },
+    {
+        numb: 2,
+        question: "Which raag: Sa Re Ma Pa Ni Sa?",
+        answer: "A. Raag Bihag",
+        options: [
+            "A. Raag Bihag",
+            "B. Raag Darbari Kanada",
+            "C. Raag Bhairavi",
+            "D. Raag Kafi"
+        ]
+    },
+    {
+        numb: 3,
+        question: "Which raag: Sa Re Ga Ma Dha Ni Sa?",
+        answer: "C. Raag Yaman Kalyan",
+        options: [
+            "A. Raag Bhairav",
+            "B. Raag Bhupali",
+            "C. Raag Yaman Kalyan",
+            "D. Raag Todi"
+        ]
+    },
+    {
+        numb: 4,
+        question: "Which raag: Sa Re Ga Ma Pa Ni Sa?",
+        answer: "A. Raag Bageshri",
+        options: [
+            "A. Raag Bageshri",
+            "B. Raag Durga",
+            "C. Raag Marwa",
+            "D. Raag Pilu"
+        ]
+    },
+    {
+        numb: 5,
+        question: "Which raag: Sa Ga Ma Dha Ni Sa?",
+        answer: "A. Raag Ahir Bhairav",
+        options: [
+            "A. Raag Ahir Bhairav",
+            "B. Raag Jog",
+            "C. Raag Desh",
+            "D. Raag Kirwani"
+        ]
+    }
+];
